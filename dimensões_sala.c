c21f3074d312a2995655b8e1a49aa3e4c012b9b5
ViniciusPinheiroLivinalli <viniciuspinheirolivinalli@gmail.com>
2024-10-23 21:29:51 -0300

    Gerar posição aspirador e imprimir matriz

--- dimensões_sala.c
+++ dimensões_sala.c
@@ -7,39 +7,8 @@ int main() {
 
 }
 
-int dimensoes(int t) {
-    // Se o valor fornecido for válido para as dimensões da matriz
-    if (t >= 0 && t < 10) {
-        int sala[t][t] = {0};
-        return sala[t][t];  // Retorna o valor na posição [valor][valor]
-    }
-
-    return -1; // Retorna -1 se o valor estiver fora das dimensões da matriz
-}
-
-
-// Função para definir as salas sujas aleatoriamente
-void definirSalasSujas(int sala[t][t], int quantidade) {
-    int x, y;
-    srand(time(0));  // Inicializa o gerador de números aleatórios com base no tempo atual
-
-    int count = 0;
-    while (count < quantidade) {
-        // Gera coordenadas aleatórias para linha e coluna
-        x = rand() % t;
-        y = rand() % t;
-
-        // Verifica se a sala já está suja, para evitar duplicações
-        if (sala[x][y] == 0) {
-            sala[x][y] = 1;  // Define a sala como suja
-            count++;  // Incrementa o contador de salas sujas definidas
-        }
-    }
-}
-
-
 // Função para gerar uma posição aleatória para o aspirador
-void gerarPosicaoAspirador(int sala[t][t], int &xAspirador, int &yAspirador) {
+void gerarPosicaoAspirador(int t, int &xAspirador, int &yAspirador) {
     int x, y;
     srand(time(0));  // Inicializa o gerador de números aleatórios com base no tempo atual
 
